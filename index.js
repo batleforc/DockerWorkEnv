0515265334f3365e4f022c7a592f6addd80143b6
maxime leriche <maxleriche.60@gmail.com>
2020-10-01 05:13:28 -0700

    Suffix

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ var docker = new Docker({socketPath: '/var/run/docker.sock'});
 
 var DevEnvDocker = require('./ModuleInterface');
 
-var Dev= new DevEnvDocker('/var/run/docker.sock');
+var Dev= new DevEnvDocker('/var/run/docker.sock','loc');
 Dev.StartDnsServer()
 //Dev.LinkDns("172.17.0.3")
