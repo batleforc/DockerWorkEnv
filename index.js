db603f66075965f31da9469535222094e5f6c1f1
Maxime Leriche <maxleriche.60@gmail.com>
2020-10-02 12:07:40 +0200

    Make the index.js more await friendly

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ var docker = new Docker({socketPath: '/var/run/docker.sock'});
 
 var DevEnvDocker = require('./ModuleInterface');
 
-var Dev= new DevEnvDocker('/var/run/docker.sock','loc');
-Dev.StartDnsServer()
+var Dev= new DevEnvDocker('/var/run/docker.sock');
+
+var test =async ()=>{
+    await Dev.StartPortainer();
+}
+test()
+//Dev.StartDnsServer()
 //Dev.LinkDns("172.17.0.3")
 //Dev.docker.getNetwork('c90446f40debcc6b9551936e1148b1afdea6d0f2b2982e550c456a819c795af6').inspect().then(console.log)
